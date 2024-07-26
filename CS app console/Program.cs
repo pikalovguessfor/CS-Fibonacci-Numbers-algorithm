6e6689e9d9df779ca8fed3c7ec2141b65bc1a4b7
pikalovvova1303 <pikalovvova1303@gmail.com>
2024-07-26 07:07:55 +0300

    Добавьте файлы проекта.

---
+++ CS app console/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_app_console
+{
+    internal class Program
+    {
+        static long FibonacciCount(long f_num, long s_num)
+        {
+
+            long final_num = f_num + s_num;
+
+            return final_num;
+        }
+
+
+        static void Main(string[] args)
+        {
+            long f_num = 0;
+            long s_num = 1;
+
+            while (true)
+            {
+                f_num = FibonacciCount(f_num, s_num);
+                s_num = FibonacciCount(f_num, s_num);
+
+
+                Console.WriteLine(f_num);
+                Console.WriteLine(s_num);
+            }
+        }
+    }
+}
